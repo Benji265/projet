eec8787eebd022c4b3de44ef38bfa00b45de9c81
Benjamin Turpin <lhp6.bturpin@gmail.com>
2021-08-05 12:46:49 +0200

    Envoie du projet

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Accueil</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand d-block d-lg-none" href="#" data-nav="accueil">Accueil</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav navLink">
+                    <li class="nav-item d-none d-lg-block d-xxl-none">
+                        <a class="nav-link active" aria-current="page" href="#" data-nav="accueil">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#" data-nav="screen">ScreenShots</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#" data-nav="insc">Inscription/Connexion</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="row sizeBody justify-content-center align-items-center">
+        <div class="row sizeBox">
+            <div class="row justify-content-center" id="accueilNav">
+                <h1 class="text-center sizeNameGame">Nom du Jeux</h1>
+                <h2 class="text-center">La porte vers de nouveaux mondes</h2>
+                <div class="col-12 col-xl-8 sizeBoxCol text-white rounded-3">
+                    <ul>
+                        <li>Bâtissez une infrastructure economique et militaire</li>
+                        <li>Livrez bataille pour remporter de précieuses ressourcs</li>
+                        <li>Armez votre flotte et renforcez votre pouvoir</li>
+                        <li>Exploitez des ressources ou pillez celles d`autres joueurs</li>
+                        <li>Commercez avec des peuples pacifiques</li>
+                        <li>Forgez des alliances et joignez vos forces pour attaquer d`autres planètes</li>
+                    </ul>
+                </div>
+                <h2 class="text-center">Créez votre empire dans l`espace</h2>
+                <div class="col-12 col-xl-8 sizeBoxCol text-white rounded-3">
+                    <ul>
+                        <li>Une stratégie épique sur navigateur et sur smartphone</li>
+                        <li>Des contenus modernes côtoient la tradition</li>
+                        <li>Gratuit, sans téléchargement</li>
+                        <li>Lancez-vous rapidement dans un univers infini</li>
+                    </ul>
+                </div>
+                <h2 class="text-center">Vos flottes combattent au milieu des étoiles</h2>
+                <div class="col-12 col-xl-8 sizeBoxCol text-white rounded-3">
+                    <p>De nombreux pionniers de l`espace se fraient un passage vers de nouveaux quadrants, fondant de nouvelles colonies et s`appropriant de précieuses ressources. Des flottes sont armées, des galaxies conquises.</p>
+                    <p>Le destin de votre peuple est entre vos mains !</p>
+                </div>
+            </div>
+            <div class="row justify-content-center d-none" id="screenNav">
+                <h1 class="text-center sizeNameGame">Test</h1>
+            </div>
+            <div class="row justify-content-center d-none" id="inscNav">
+                <h1 class="text-center sizeNameGame">Coucou</h1>
+            </div>
+        </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
+    </script>
+    <script src="assets/js/nav.js"></script>
+</body>
+
+</html>
---
+++ template/left_menu.php
@@ -0,0 +1,181 @@
+<div id='menu'>
+    <br>
+    <table>
+        <tr>
+            <td colspan="2" style="border-top: 1px #545454 solid">
+                <div class="red">XNova</div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" background="{dpath}img/bg1.gif">
+                <center>{devlp}</center>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="overview.php">{Overview}</a></div>
+            </td>
+        </tr>
+        <tr>
+
+            <td height="1px" colspan="2" style="background-color:#FFFFFF"></td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="buildings.php">{Buildings}</a></div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="buildings.php?mode=research">{Research}</a></div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="buildings.php?mode=fleet">{Shipyard}</a></div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="buildings.php?mode=defense">{Defense}</a></div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="officier.php">{Officiers}</a></div>
+            </td>
+        </tr>
+        <tr>
+
+            <td colspan="2" background="{dpath}img/bg1.gif">
+                <center>{navig}</center>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="alliance.php">{Alliance}</a></div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="fleet.php">{Fleet}</a></div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="messages.php">{Messages}</a></div>
+            </td>
+        </tr>
+        <tr>
+
+            <td colspan="2" background="{dpath}img/bg1.gif">
+                <center>{observ}</center>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="galaxy.php?mode=0">{Galaxy}</a></div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="imperium.php">{Imperium}</a></div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="resources.php">{Resources}</a></div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="techtree.php">{Technology}</a></div>
+            </td>
+        </tr>
+        <tr>
+
+            <td height="1px" colspan="2" style="background-color:#FFFFFF"></td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="records.php">{Records}</a></div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="stat.php?range={user_rank}">{Statistics}</a></div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="search.php">{Search}</a></div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="banned.php">{blocked}</a></div>
+            </td>
+        </tr>
+        <tr>
+            
+            <td colspan="2" background="{dpath}img/bg1.gif">
+                <center>{commun}</center>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="#" onClick="f('buddy.php', '');" accesskey="c">{Buddylist}</a></div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="chat.php">{Chat}</a></div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="{forum_url}">{Board}</a></div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="add_declare.php">{multi}</a></div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="rules.php">{Rules}</a></div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="contact.php">{Contact}</a></div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="options.php">{Options}</a></div>
+            </td>
+        </tr>
+        <tr>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div><a href="javascript:top.location.href='logout.php'" style="color:red">{Logout}</a></div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" background="{dpath}img/bg1.gif">
+                <center>{infog}</center>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <div>
+                    <center><a href="credit.php">XNova Team</a><br>&copy;
+                        Copyright 2008</center>
+                </div>
+            </td>
+        </tr>
+    </table>
+</div>
