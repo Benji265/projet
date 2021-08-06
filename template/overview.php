8ef291b2914cd561fddbb9d0f769b8cdf085e2ee
Benjamin Turpin <lhp6.bturpin@gmail.com>
2021-08-06 13:19:30 +0200

    ajout de l'affichage planette

---
+++ template/overview.php
@@ -0,0 +1,40 @@
+<table width="519">
+    <tr>
+        <td class="c text-center" style="border-top: 1px #545454 solid" colspan="4">{Planet} "{planet_name}" ({user_username})</td>
+    </tr>
+    <tr>
+        <th>{Server_time}</th>
+        <th colspan="3">
+            <div>{Time}</div>
+        </th>
+    </tr>
+    <tr>
+        <th>{MembersOnline}</th>
+        <th colspan="3">{NumberMembersOnline}</th>
+    </tr>
+    <tr>
+        <td colspan="4" class="c text-center">{Events}</td>
+    </tr>
+    <tr>
+        <th>{moon_img}<br>{moon}</th>
+        <th colspan="2"><img src="assets/img/planeten/eisplanet01.jpg" height="200" width="200"></th>
+        <th class="s">
+            <table class="s">
+                <tr>{anothers_planets}</tr>
+            </table>
+        </th>
+    </tr>
+    <tr>
+        <th>{Diameter}</th>
+        <th colspan="3">{planet_diameter} km</th>
+    </tr>
+    <th>{Developed_fields}</th>
+    <th colspan="3" align="center">
+        <div style="border: 1px solid rgb(153, 153, 255); width: 400px;">
+            <div>{case_pourcentage}</div>
+    </th>
+    <tr>
+        <th>{Temperature}</th>
+        <th colspan="3">{tempmin}/{tempmax}</th>
+    </tr>
+</table>
